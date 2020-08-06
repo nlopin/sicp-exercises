adee0a6155ef0511338d983c1768283aff0a0eea
Nikolai Lopin <nikolai.lopin@mytaxi.com>
2020-08-07 00:10:49 +0200

    1.3: extract pattern to fn

--- 1.3.clj
+++ 1.3.clj
@@ -1,9 +1,11 @@
 (defn square [x] (* x x))
 
+(defn sum-squares [x y] (+ (square x) (square y)))
+
 (defn sum-of-max-squares [x y z]
-  (cond (and (< x y) (< x z)) (+ (square y) (square z))
-        (and (< y x) (< y z)) (+ (square x) (square z))
-        :else                 (+ (square x) (square y))))
+  (cond (and (< x y) (< x z)) (sum-squares y z)
+        (and (< y x) (< y z)) (sum-squares x z)
+        :else                 (sum-squares x y)))
 
 (println (sum-of-max-squares 5 10 15))
 (println (sum-of-max-squares 15 5 10))
