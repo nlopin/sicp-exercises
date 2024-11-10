754ca347841aeb20307ddf1d8e8f519e57e2b5f4
Nikolai Lopin <code@lopin.me>
2024-11-10 13:39:52 +0100

    add 1.31

---
+++ racket/1.31.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (product a b term next)
+  (if (> a b)
+    1
+    (* (term a) (product (next a) b term next))
+  )
+)
+
+(define (product-fast a b term next)
+  (define (product-iter a result)
+    (if (> a b)
+      result
+      (product-iter (next a) (* (term a) result))
+  ))
+  (product-iter a 1)
+)
+
+(define (identity x) x)
+(define (next x) (+ 1 x))
+(product-fast 1 5 identity next)
